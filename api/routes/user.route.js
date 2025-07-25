61dfa27ddf5364ba29bf0afd01842d8158926049
shinaayomi <lateef718@gmail.com>
2025-07-25 05:50:47 +0100

    create user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { test, updateUser } from "../controller/user.controller.js";
+import { verifyTokken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.patch("/update/:id", updateUser);
+router.patch("/update/:id", verifyTokken, updateUser);
 
 export default router;

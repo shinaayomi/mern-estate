a752fbe9437f4a448515573cc33b88860a3d3d7b
shinaayomi <lateef718@gmail.com>
2025-07-31 14:26:53 +0100

    complete delete user listing

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,9 +1,13 @@
 import express from "express";
 import { verifyTokken } from "../utils/verifyUser.js";
-import { createListing } from "../controller/listing.controller.js";
+import {
+  createListing,
+  deleteListing,
+} from "../controller/listing.controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyTokken, createListing);
+router.delete("/delete/:id", verifyTokken, deleteListing);
 
 export default router;
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -28,8 +28,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
-  const [showListingsError, setShowListingsError] = useState(false)
-  const [userListings, setUserLIstings] = useState([])
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserLIstings] = useState([]);
   const dispatch = useDispatch();
 
   // Firebase storage
@@ -129,18 +129,36 @@ export default function Profile() {
 
   const handleShowListings = async () => {
     try {
-      setShowListingsError(false)
-      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      setShowListingsError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingsError(true);
+        return;
+      }
+      setUserLIstings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  };
+
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
       const data = await res.json()
       if (data.success === false) {
-        setShowListingsError(true)
+        console.log(data.message)
         return
       }
-      setUserLIstings(data)
+
+      setUserLIstings((prev) => prev.filter(listing => listing._id !== listingId))
     } catch (error) {
-      setShowListingsError(true)
+      console.error(error.message);
     }
-  }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
@@ -222,28 +240,50 @@ export default function Profile() {
       <p className="text-green-700 text-sm mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
       </p>
-      <button onClick={handleShowListings} className="text-green-700 w-full">Show LIstings</button>
-      {showListingsError && <p className="text-red-700 mt-5">Error showing listings</p>}
+      <button onClick={handleShowListings} className="text-green-700 w-full">
+        Show LIstings
+      </button>
+      {showListingsError && (
+        <p className="text-red-700 mt-5">Error showing listings</p>
+      )}
 
-      {userListings && userListings.length > 0 &&
+      {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
-            <div key={listing._id} className="border rounded-lg p-3 flex justify-between items-center gap-4">
+            <div
+              key={listing._id}
+              className="border rounded-lg p-3 flex justify-between items-center gap-4"
+            >
               <Link to={`/listings/${listing._id}`}>
-                <img src={listing.imageUrls[0]} alt="listing cover" className="h-16 w-16 object-contain" />
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-16 w-16 object-contain"
+                />
               </Link>
-              <Link to={`/listings/${listing._id}`} className="flex-1 text-slate-700 font-semibold hover:underline truncate">
-                <p >{listing.name}</p>
+              <Link
+                to={`/listings/${listing._id}`}
+                className="flex-1 text-slate-700 font-semibold hover:underline truncate"
+              >
+                <p>{listing.name}</p>
               </Link>
 
               <div className="flex flex-col items-center">
-                <button className="text-red-700 uppercase">Delete</button>
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase"
+                >
+                  Delete
+                </button>
                 <button className="text-green-700 uppercase">Edit</button>
               </div>
             </div>
-          ))}</div>
-      }
+          ))}
+        </div>
+      )}
     </div>
   );
 }

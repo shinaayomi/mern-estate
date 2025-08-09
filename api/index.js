3023497a10dc3aea3bc927aaabd20a6dfb1bef25
shinaayomi <lateef718@gmail.com>
2025-08-09 12:47:11 +0100

    Add pre installation

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 // dotenv.config();
 dotenv.configDotenv();
 
@@ -17,6 +18,8 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -31,6 +34,13 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  // res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 // Middleware
 app.use((err, req, res, next) => {
